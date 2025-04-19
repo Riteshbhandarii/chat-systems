3ba2fd0a7b38ebb5865cea4289ac60eb66587aff
Riteshbhandarii <Bhanrit@gmail.com>
2025-04-20 01:25:46 +0300

    testing

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,29 +1,19 @@
-"""
-ASGI config for chat_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-# accesses enviroment variables
 # chat_project/asgi.py
 import os
 import django
 from django.core.asgi import get_asgi_application
-
-# THIS MUST COME FIRST
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
-django.setup()  # Initialize Django
-
-# Then import your WebSocket components
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from messaging.routing import websocket_urlpatterns
+from django.conf import settings  # Import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
+    "channel": settings.CHANNEL_LAYERS["default"],  # Use the configured channel layer
 })
